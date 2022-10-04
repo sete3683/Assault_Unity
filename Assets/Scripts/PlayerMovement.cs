082f93dfd8089943154b09a726eebcc02527916d
sete3683 <sete3683@gmail.com>
2022-10-05 01:09:19 +0900

    Day 2

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float movementSpeed;
+    [SerializeField, Range(0, 10)] private float xMovementRange;
+    [SerializeField, Range(0, 5)] private float yMovementRange;
+
+    [SerializeField] private float xRotateSpeed;
+    [SerializeField] private float zRotateSpeed;
+
+
+    private Vector2 movementInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float nextXPos = Mathf.Clamp(transform.localPosition.x + movementInput.x, -xMovementRange, xMovementRange);
+        float nextYPos = Mathf.Clamp(transform.localPosition.y + movementInput.y, -yMovementRange, yMovementRange);
+
+        transform.localPosition = new Vector3(nextXPos, nextYPos, transform.localPosition.z);
+
+        float nextXRot = transform.localPosition.y * xRotateSpeed;
+        float nextZRot = transform.localPosition.x * zRotateSpeed;
+
+        transform.localRotation = Quaternion.Euler(nextXRot, 0f, nextZRot);
+    }
+
+    void OnMove(InputValue value)
+    {
+        movementInput = value.Get<Vector2>() * movementSpeed * Time.deltaTime;
+    }
+}
